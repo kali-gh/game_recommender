80929ca6fef53054354b2d8c724c1936d0672640
kali <kareemali01@gmail.com>
2025-05-10 20:57:13 -0400

    updating bayesian with sklearn

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-### What does this do?
+### Overview
 
 We build a simple game recommendation engine based on a simple content based approach. 
 The user inputs their preferred games, by rating games from 1 to 10. Feature data is then used to predict this score. 
 The feature data contains (1) basic attributes about the game and 
 (2) simple topic model embeddings which describe the game.
-We build a simple neural network to predict the score based on these features and run inference on recent releases. 
+We build different models to predict the score based on these features and run inference on recent releases. The user can
+also give a input game and the system will output the most similar game along with that games estimated rating.
 Our model validation uses a train/validation/test approach and we measure the performance on validation for this and
 then run inference on the test set. 
 
 ### Diagram
-![alt text](assets/game_recommender.png)
+![alt text](assets/game_recommender_v2.png)
+
+
+### Running
+
+After setting up the input data (which is not provided in this repo), run :
+
+```
+python train_bayesian.py
+```
+
 
 ### Environment set up
 
@@ -19,18 +30,9 @@ source venv/bin/activate
 pip install -r requirements.txt
 ```
 
-### Running
 
-After setting up the input data (which is not provided in this repo), run :
-
-```
-python labels.py
-python train.py
-```
-
+### Building as a wheel
 ```
 python setup.py bdist_wheel
 ```
-
-### Results
 
--- train_bayesian.py
+++ train_bayesian.py
@@ -1,18 +1,15 @@
 import torch
-import json
 import logging
-import numpy as np
 import os
-
+import pandas as pd
+import numpy as np
 from pathlib import Path
-from scipy.stats import multivariate_normal
 
-from bayesian_normal_regression.bayes_normal import BayesNormal
-from bayesian_normal_regression.dist import Normal
-
-from game_recommender.libs_torch import create_loader
 from game_recommender.labels import get_labels, load_data
 from game_recommender.parameters import Params
+from bayesian_normal.bayes_normal_known_var import BayesNormalEstimator
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -27,126 +24,99 @@ df_labels = get_labels(params['pca_dim'])
 output_data_dir_scores = os.path.join(params['output_data_dir'], params['output_data_subdir_scores'])
 Path(output_data_dir_scores).mkdir(parents=True, exist_ok=True)
 
-def train():
+x_cols = [
+    'review_pct_overall',
+    'review_count_overall',
+    'has_reviews',
+    'has_extra_content',
+    'x_content_0',
+    'x_content_1',
+    'x_content_2',
+    'x_content_3',
+    'x_content_4',
+    'x_genre_0',
+    'x_genre_1',
+    'x_genre_2',
+    'x_genre_3',
+    'x_genre_4'
+]
 
-    logger.info("Loading data")
-    df_train_val, df_train, df_val, df_test, x_cols = load_data()
-    input_size = len(x_cols)
+def run_inference_test(
+        df):
 
-    batch_size = params['batch_size']
+    cond_train_val = ~df.rating.isnull()
+    df_train_val_local = df[cond_train_val].copy()
+    cond_test = df.rating.isnull()
+    df_test_local = df[cond_test].copy()
 
-    if params['use_all_data']:
-        logger.info("Using all data for training")
-        train_loader = create_loader(df_train_val, x_cols, batch_size)
-    else:
-        logger.info("only using training")
-        train_loader = create_loader(df_train, x_cols, batch_size)
+    logger.info(x_cols)
+    X = df_train_val_local[x_cols].values
 
-    val_loader = create_loader(df_val, x_cols, batch_size)
+    y = df_train_val_local.rating.values
+    y = y.reshape(-1, 1)
 
-    logger.info("Training model")
-
-    logger.info("Running inference on test data")
-    logger.info(len(df_test))
-    test_loader = create_loader(df_test, x_cols, batch_size, shuffle=False)
-
-    #### parameters
-    n_param = input_size
+    n_param = len(x_cols)
     prior_alpha = 2.0
     prior_mean = np.zeros(shape=(n_param, 1))
     prior_cov = (1 / prior_alpha) * np.identity(n_param)
 
-    w0_true = -0.7
-    w1_true = 0.5
-    precision_y = 25
-    std_dev_y = np.sqrt(1 / precision_y)
+    logger.info(f"shape of x {X.shape}, shape of y {y.shape}")
 
-    logger.debug("PARAMETERS")
-    logger.debug(
-        f"{prior_alpha=}\n"
-        f"{prior_mean=}\n"
-        f"{prior_cov=}\n"
+    bn = BayesNormalEstimator(variance=1, prior_mean=prior_mean, prior_cov=prior_cov, predict_thompson_sample=True)
+    fitted = bn.fit(X, y)
+    df_train_val_local.loc[:, 'score'] = fitted.predict(X)
 
-        f"{w0_true=}\n"
-        f"{w1_true=}\n"
+    X_test = df_test_local[x_cols].values
+    logger.info(f"shape of x TEST {X_test.shape},")
 
-        f"{precision_y=}\n"
-        f"{std_dev_y=}\n"
-    )
+    df_test_local.loc[:, 'score'] = fitted.predict(X_test)
 
-    ##### main script
-    logger.debug("Initializing prior")
-    prior = Normal(mean=prior_mean, cov=prior_cov)
-    logger.debug(prior)
+    df = pd.concat([df_train_val_local, df_test_local], axis=0)
+    logger.info(f"len of df is {len(df)}")
 
+    df.sort_values(by='score', ascending=False, inplace=True)
+    df.to_csv('df_predict.csv')
 
-    def model(W_array, X_array):
-        return W_array[0] + W_array[1] * X_array
+    return fitted
 
-    bn = BayesNormal(variance=1, prior_mean=prior_mean, prior_cov=prior_cov)
-
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        bn.add_observations(X=inputs.numpy(), y=targets.numpy())
-
-        logger.debug('posterior')
-        logger.debug(bn.posterior)
-
-    X_val, y_val = df_val.loc[:, x_cols], df_val.loc[:, 'rating']
-    y_pred = X_val @ bn.posterior.mean
-
-    from sklearn.metrics import root_mean_squared_error
-
-    rmse = root_mean_squared_error(y_true=y_val, y_pred=y_pred)
-    logger.info('rmse on validation')
-    logger.info(rmse)
-
-    logger.info("Running inference on test data")
-    logger.info(len(df_test))
-    test_loader = create_loader(df_test, x_cols, batch_size, shuffle=False)
-    X_test = df_test.loc[:, x_cols]
-    inferences = X_test @ bn.posterior.mean
-    df_test['score'] = inferences
-
-    logger.info("Outputing scores")
-    cols_score = ['appid', 'title', 'score']
-    df_score_only = df_test[cols_score].copy()
-    df_test.to_csv(f"{output_data_dir_scores}/df_test.csv")
-    df_score_only.sort_values(by='score', ascending=False, inplace=True)
-
-    with open(f"{params['user_input_data_dir']}/sample_ids_recent_releases.json", 'r') as f:
-        recent_release_ids = json.load(f)
-
-    df_score_only.loc[:, 'recent_release'] = df_score_only.appid.isin(recent_release_ids)
-
-    df_score_only.to_csv(
-        f"{output_data_dir_scores}/df_score_only_bayesian_mean.csv",
-        index=False)
-
-    logger.info("Running inference on test data")
-    logger.info(len(df_test))
-    test_loader = create_loader(df_test, x_cols, batch_size, shuffle=False)
-    X_test = df_test.loc[:, x_cols]
-    sample_posterior = multivariate_normal(mean=bn.posterior.mean.flatten(), cov=bn.posterior.cov).rvs(size=1)
-    sample_posterior = np.reshape(sample_posterior, (-1, 1))
-    inferences = X_test @ sample_posterior
-    df_test['score'] = inferences
-
-    logger.info("Outputing scores")
-    cols_score = ['appid', 'title', 'score']
-    df_score_only = df_test[cols_score].copy()
-    df_test.to_csv(f"{output_data_dir_scores}/df_test.csv")
-    df_score_only.sort_values(by='score', ascending=False, inplace=True)
-
-    with open(f"{params['user_input_data_dir']}/sample_ids_recent_releases.json", 'r') as f:
-        recent_release_ids = json.load(f)
-
-    df_score_only.loc[:, 'recent_release'] = df_score_only.appid.isin(recent_release_ids)
-
-    df_score_only.to_csv(
-        f"{output_data_dir_scores}/df_score_only_bayesian_thompson.csv",
-        index=False)
-
-    return rmse
 
 if __name__ == '__main__':
-    train()
+    df = pd.read_csv(
+        os.path.join(params['output_data_dir'], params['output_data_subdir_labels'], 'df_labels_model_ready_current.csv')
+    )
+
+    cond_train_val = ~df.rating.isnull()
+    df_train_val_local = df[cond_train_val].copy()
+
+    df_train_val, df_test = train_test_split(df_train_val_local, test_size=0.2)
+
+    X_train_val = df_train_val[x_cols].values
+    y_train_val = df_train_val['rating'].values.reshape(-1,1)
+
+    X_test = df_test[x_cols].values
+    y_test = df_test['rating'].values.reshape(-1,1)
+
+    n_param = len(x_cols)
+    prior_alpha = 2.0
+    prior_mean = np.zeros(shape=(n_param, 1))
+    prior_cov = (1 / prior_alpha) * np.identity(n_param)
+
+    logger.info(f"shape of x {X_train_val.shape}, shape of y {y_train_val.shape}")
+
+    bn = BayesNormalEstimator(
+        variance=1,
+        prior_mean=prior_mean,
+        prior_cov=prior_cov,
+        predict_thompson_sample=True)
+
+    scores = np.sqrt(-1*
+        cross_val_score(
+            bn,
+            X_train_val,
+            y_train_val,
+            cv=10,
+            scoring='neg_mean_squared_error')
+    )
+
+    logger.info(f"Scores : {scores}")
+    logger.info(f"Mean score : {scores.mean()}")
